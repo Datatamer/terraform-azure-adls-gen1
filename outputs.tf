af15390cd0de3e4d762fd9ba2dc4d55a0d9279a7
Dominick Olivito <dominick.olivito@gmail.com>
2020-05-12 11:38:25 -0400

    fix typo

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,6 @@ output "adls_id" {
 }
 
 output "adls_firewall_rule_id" {
-    value = azurerm_data_lake_store.allowed_addresses.id
+    value = azurerm_data_lake_store_firewall_rule.allowed_addresses.id
     description = "The ID of the ADLS Gen 1 firewall rule"
 }
